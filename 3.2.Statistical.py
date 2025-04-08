faf21a6c676a1a134e3d5f128364e04f293bf7cc
lequycong204 <152189283+lequycong204@users.noreply.github.com>
2025-04-08 16:43:05 +0700

    Add files via upload

---
+++ 3.2.Statistical.py
@@ -0,0 +1,12 @@
+import numpy as np
+def mean(s):
+    s = np.array(s)
+    return np.mean(s)
+
+def variance(s):
+    s = np.array(s)
+    return np.var(s)
+
+def standarddeviation(s):
+    s = np.array(s)
+    return np.std(s)
